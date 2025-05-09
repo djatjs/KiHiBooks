2fb2f023a0c0f98a9ba6933ba4ed84beaae8e0a5
p-jy <jypark1107@hanmail.net>
2025-05-09 11:16:43 +0900

    수정

--- kihibooks/src/main/java/kr/kh/kihibooks/config/MyBatisConfig.java
+++ kihibooks/src/main/java/kr/kh/kihibooks/config/MyBatisConfig.java
@@ -1,6 +1,5 @@
 package kr.kh.kihibooks.config;
 
-
 import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -13,24 +12,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @Configuration
-@MapperScan ("kr.kh.kihibooks.dao")
+@MapperScan("kr.kh.kihibooks.dao")
 public class MyBatisConfig {
 
 	@ConfigurationProperties(prefix = "spring.datasource")
-    @Bean
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
-    }
+	@Bean
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().build();
+	}
 
 	@Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource);
-        sessionFactory.setMapperLocations(
-            new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*.xml")
-        );
-         // TypeAlias 적용
-        sessionFactory.setTypeAliasesPackage("kr.kh.kihibooks.model.vo");  // 여기에 패키지 경로 지정
-        return sessionFactory.getObject();
-    }
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+		sessionFactory.setMapperLocations(
+				new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*.xml"));
+		// TypeAlias 적용
+		sessionFactory.setTypeAliasesPackage("kr.kh.kihibooks.model.vo"); // 여기에 패키지 경로 지정
+		return sessionFactory.getObject();
+	}
 }
--- kihibooks/src/main/java/kr/kh/kihibooks/dao/UserDAO.java
+++ kihibooks/src/main/java/kr/kh/kihibooks/dao/UserDAO.java
@@ -1,5 +1,5 @@
 package kr.kh.kihibooks.dao;
 
-public class UserDAO {
+public interface UserDAO {
 	
 }
