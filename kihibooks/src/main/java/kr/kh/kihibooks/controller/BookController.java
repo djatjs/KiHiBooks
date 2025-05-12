19bfbd6bd04e9183d448d485ab56df944cd2ee20
irruyan <irruyan@gmail.com>
2025-05-12 15:52:22 +0900

    BookController 구현 중

---
+++ kihibooks/src/main/java/kr/kh/kihibooks/controller/BookController.java
@@ -0,0 +1,25 @@
+package kr.kh.kihibooks.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import kr.kh.kihibooks.model.vo.BookVO;
+import kr.kh.kihibooks.service.BookService;
+
+
+@Controller
+public class BookController {
+    
+    @Autowired
+    private BookService bookService;
+    
+    @GetMapping("/realtime")
+    public List<BookVO> getMethodName(@RequestParam String param) {
+        return bookService.getTopBooks();
+    }
+    
+}
