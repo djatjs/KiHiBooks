3c05b1f8f2512eddcc020ed7842c0dad4e0dd42b
Sunil <68678050+djatjs@users.noreply.github.com>
2025-05-20 21:44:09 +0900

    에디터 하는중

--- kihibooks/src/main/java/kr/kh/kihibooks/controller/PublisherContoller.java
+++ kihibooks/src/main/java/kr/kh/kihibooks/controller/PublisherContoller.java
@@ -3,12 +3,17 @@ package kr.kh.kihibooks.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
-public class Publisher {
+public class PublisherContoller {
     @GetMapping("/publisher/dashboard")
     public String publisherDashboard() {
         return "/publisher/dashboard";
     }
+
+    @GetMapping("publisher/editors")
+    public String editors() {
+        return "publisher/editors";
+    }
+    
     
 }
