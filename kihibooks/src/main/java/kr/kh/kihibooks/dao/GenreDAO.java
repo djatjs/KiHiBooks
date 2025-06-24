c7ea79c7d4bf375be4b401530b89df5cad9592e0
p-jy <jypark1107@hanmail.net>
2025-06-24 23:27:46 +0900

    Revert "."
    This reverts commit e667feb5da53189ccf7a5365f0bdf78424b32660.

--- kihibooks/src/main/java/kr/kh/kihibooks/dao/GenreDAO.java
+++ kihibooks/src/main/java/kr/kh/kihibooks/dao/GenreDAO.java
@@ -2,14 +2,12 @@ package kr.kh.kihibooks.dao;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Param;
-
 import kr.kh.kihibooks.model.vo.BookVO;
 import kr.kh.kihibooks.model.vo.KeywordVO;
 
 public interface GenreDAO {
 
-	List<KeywordVO> selectRandomKeywords(@Param("limit")int limit);
+	List<KeywordVO> selectRandomKeywords(int limit);
 
 
     
